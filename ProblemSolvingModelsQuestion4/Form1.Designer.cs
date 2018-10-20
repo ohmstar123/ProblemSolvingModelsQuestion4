68c5a4b5e804de5f025be76e93f4dcf4e4601cf8
Ohm Patel <ohmpatel86@ed.amdsb.ca>
2018-10-19 23:13:12 -0400

    done

--- ProblemSolvingModelsQuestion4/Form1.Designer.cs
+++ ProblemSolvingModelsQuestion4/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProblemSolvingModelsQuestion4
 {
-    partial class Form1
+    partial class outputLabel
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.backgroungButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.continueButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // backgroungButton
+            // label1
             // 
-            this.backgroungButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.backgroungButton.Location = new System.Drawing.Point(2, -2);
-            this.backgroungButton.Name = "backgroungButton";
-            this.backgroungButton.Size = new System.Drawing.Size(798, 451);
-            this.backgroungButton.TabIndex = 0;
-            this.backgroungButton.Text = "Click on window to start";
-            this.backgroungButton.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(320, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click on the window to start";
             // 
-            // Form1
+            // continueButton
+            // 
+            this.continueButton.Location = new System.Drawing.Point(304, 317);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(176, 52);
+            this.continueButton.TabIndex = 1;
+            this.continueButton.Text = "Continue";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Visible = false;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // outputLabel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.backgroungButton);
-            this.Name = "Form1";
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.label1);
+            this.Name = "outputLabel";
             this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button backgroungButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button continueButton;
     }
 }
 
--- ProblemSolvingModelsQuestion4/Form1.cs
+++ ProblemSolvingModelsQuestion4/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ProblemSolvingModelsQuestion4
 {
-    public partial class Form1 : Form
+    public partial class outputLabel : Form
     {
-        public Form1()
+        public outputLabel()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+
+            BackColor = Color.Black;
+
+            Refresh();
+            Thread.Sleep(3000);
+            Refresh();
+
+            label1.Text = "You";
+            continueButton.Visible = true;
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            //"You should not have done that"
+            //Put delay of 1 second between each word
+
+            Refresh();
+            Thread.Sleep(1000);
+            label1.Text = "You should";
+
+            Refresh();
+            Thread.Sleep(1000);
+            label1.Text = "You should not";
+
+            Refresh();
+            Thread.Sleep(1000);
+            label1.Text = "You should not have";
+
+            Refresh();
+            Thread.Sleep(1000);
+            label1.Text = "You should not have done";
+
+            Refresh();
+            Thread.Sleep(1000);
+            label1.Text = "You should not have done that";
+
+            Refresh();
+            Thread.Sleep(2500);
+
+            this.Close();
         }
     }
 }
